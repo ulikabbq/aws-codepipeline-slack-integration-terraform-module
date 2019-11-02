357c2cce577c3c2ffaabf08487e1b49256e5d7e8
thositha@gmail.com <Code@2018!>
2019-11-02 11:54:13 -0400

    HCL 12 conversion and Node runtime upgraded from 8x to 10x.

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # This HACK installs NPM packages on every run so we get lambda function ready to be published
 resource "null_resource" "pull_and_install_github_repo" {
   triggers {
-    force_run = "${uuid()}"
+    force_run = uuid()
   }
   provisioner "local-exec" {
     command = "cd ${path.module}/aws-codepipeline-slack-integration && npm install -production"
@@ -41,7 +41,7 @@ EOF
 # IAM Policy for Lambda function
 resource "aws_iam_role_policy" "lambda_role_policy" {
   name = "${var.APP_NAME}-slack-integration-lambda-role-policy"
-  role = "${aws_iam_role.lambda_role.id}"
+  role = aws_iam_role.lambda_role.id
 
   policy = <<EOF
 {
@@ -74,21 +74,21 @@ EOF
 }
 
 resource "aws_lambda_function" "lambda" {
-  filename         = "${data.archive_file.lambda_zip.output_path}"
-  source_code_hash = "${data.archive_file.lambda_zip.output_base64sha256}"
+  filename         = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
   description      = "Posts a message to Slack channel '${var.SLACK_CHANNEL}' every time there is an update to codepipeline execution."
   function_name    = "${var.APP_NAME}-slack-integration-lambda"
-  role             = "${aws_iam_role.lambda_role.arn}"
+  role             = aws_iam_role.lambda_role.arn
   handler          = "handler.handle"
-  runtime          = "nodejs8.10"
-  timeout          = "${var.LAMBDA_TIMEOUT}"
-  memory_size      = "${var.LAMBDA_MEMORY_SIZE}"
+  runtime          = "nodejs10.x"
+  timeout          = var.LAMBDA_TIMEOUT
+  memory_size      = var.LAMBDA_MEMORY_SIZE
 
   environment {
     variables = {
-      "SLACK_WEBHOOK_URL" = "${var.SLACK_WEBHOOK_URL}"
-      "SLACK_CHANNEL"     = "${var.SLACK_CHANNEL}"
-      "RELEVANT_STAGES"   = "${var.RELEVANT_STAGES}"
+      "SLACK_WEBHOOK_URL" = var.SLACK_WEBHOOK_URL
+      "SLACK_CHANNEL"     = var.SLACK_CHANNEL
+      "RELEVANT_STAGES"   = var.RELEVANT_STAGES
     }
   }
 }
@@ -96,7 +96,7 @@ resource "aws_lambda_function" "lambda" {
 # Alias pointing to latest for Lambda function
 resource "aws_lambda_alias" "lambda_alias" {
   name             = "latest"
-  function_name    = "${aws_lambda_function.lambda.arn}"
+  function_name    = aws_lambda_function.lambda.arn
   function_version = "$LATEST"
 }
 
@@ -125,14 +125,14 @@ PATTERN
 resource "aws_lambda_permission" "allow_cloudwatch" {
   statement_id   = "AllowExecutionFromCloudWatch"
   action         = "lambda:InvokeFunction"
-  function_name  = "${aws_lambda_function.lambda.function_name}"
+  function_name  = aws_lambda_function.lambda.function_name
   principal      = "events.amazonaws.com"
-  source_arn     = "${aws_cloudwatch_event_rule.pipeline_state_update.arn}"
-  qualifier      = "${aws_lambda_alias.lambda_alias.name}"
+  source_arn     = aws_cloudwatch_event_rule.pipeline_state_update.arn
+  qualifier      = aws_lambda_alias.lambda_alias.name
 }
 
 # Map event rule to trigger lambda function
 resource "aws_cloudwatch_event_target" "lambda_trigger" {
-  rule = "${aws_cloudwatch_event_rule.pipeline_state_update.name}"
-  arn  = "${aws_lambda_alias.lambda_alias.arn}"
+  rule = aws_cloudwatch_event_rule.pipeline_state_update.name
+  arn  = aws_lambda_alias.lambda_alias.arn
 }
